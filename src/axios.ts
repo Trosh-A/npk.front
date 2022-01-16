7af58e59315ae43c80b88b283dd8c6a0091f16f4
Troshkin_Andrew <troshkin-a@bk.ru>
2022-01-16 11:11:04 +0300

    Edit base url for server

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
 const ax: AxiosInstance = axios.create({
-  baseURL: `${process.env.VUE_APP_BASE_SERVER_URL}`,
   headers: {
     "Content-type": "application/json",
   },
